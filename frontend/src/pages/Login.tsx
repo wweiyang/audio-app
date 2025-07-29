b3381f6854faa5a9d881f97e85e7728d5b7f049b
wweiyang <nwy290@gmail.com>
2025-07-29 16:15:30 +0800

    Replace default content with new placeholder content, add css modules, scss, basic Login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./login.module.scss";
+
+const Login: React.FC = () => {
+  return (
+    <div className={styles.login}>
+      <h2>Login</h2>
+    </div>
+  );
+};
+
+export default Login;
