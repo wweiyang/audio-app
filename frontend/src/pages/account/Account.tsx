722b423703cebb439186aa1d9d12414e8f1e16fd
wweiyang <nwy290@gmail.com>
2025-07-29 16:41:41 +0800

    Move pages to their own folders

--- frontend/src/pages/account/Account.tsx
+++ frontend/src/pages/account/Account.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout } from "antd";
+import styles from "./account.module.scss";
 
 const { Content } = Layout;
 
 const Account: React.FC = () => {
   return (
     <Layout>
-      <Content style={{ padding: "20px" }}>
+      <Content className={styles.account}>
         <h1>Account Management</h1>
       </Content>
     </Layout>
--- frontend/src/pages/audio/Audio.tsx
+++ frontend/src/pages/audio/Audio.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-
 import { Layout } from "antd";
+import styles from "./audio.module.scss";
 
 const { Content } = Layout;
 
 const Audio: React.FC = () => {
   return (
     <Layout>
-      <Content style={{ padding: "20px" }}>
+      <Content className={styles.audio}>
         <h1>Manage Your Audio Files</h1>
       </Content>
     </Layout>
