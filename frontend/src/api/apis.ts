cb6d0e555b312fcc582c44e98339db5de548ec46
wweiyang <nwy290@gmail.com>
2025-07-30 22:32:12 +0800

    Add uploadAudio api

--- frontend/src/api/apis.ts
+++ frontend/src/api/apis.ts
@@ -65,15 +65,15 @@ export const deleteUser = async (userId: number, token: string) => {
 };
 
 // // Audio Management API
-// export const uploadAudio = async (formData, token) => {
-//   const response = await axios.post(`${API_URL}/audio/upload`, formData, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       "Content-Type": "multipart/form-data",
-//     },
-//   });
-//   return response.data;
-// };
+export const uploadAudio = async (formData, token) => {
+  const response = await axios.post(`${API_URL}/audio/upload`, formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
+};
 
 // export const getUserAudio = async (token) => {
 //   const response = await axios.get(`${API_URL}/audio`, {
