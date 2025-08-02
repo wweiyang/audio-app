8322cb37b70307078616a31ba996b00e64fcb478
wweiyang <nwy290@gmail.com>
2025-08-02 23:05:36 +0800

    Fix AudioList elements not responsive for small screens

---
+++ frontend/src/components/AudioList/AudioList.tsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { List, Button, Card, message, Flex, Typography } from "antd";
+import { getUserAudio, playAudio } from "../../api/apis";
+import { SyncOutlined } from "@ant-design/icons";
+import { TOKEN_KEY } from "../../authentication/AuthProvider";
+import { AudioInfo } from "../../api/types";
+import styles from "./audio-list.module.scss";
+
+const { Text } = Typography;
+
+const AudioList: React.FC = () => {
+  const [audioFiles, setAudioFiles] = useState<AudioInfo[]>([]);
+  const [selectedAudio, setSelectedAudio] = useState<AudioInfo | null>(null);
+  const [audioSource, setAudioSource] = useState<string | null>(null);
+
+  const fetchAudioFiles = async () => {
+    try {
+      const data = await getUserAudio(localStorage.getItem(TOKEN_KEY) || "");
+
+      setAudioFiles(data);
+    } catch (error) {
+      console.error("Error fetching audio files:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAudioFiles();
+  }, []);
+
+  const handlePlay = async (audio: AudioInfo) => {
+    const authToken = localStorage.getItem(TOKEN_KEY);
+    if (!authToken) return;
+
+    try {
+      const audioSrc = await playAudio(audio.id, authToken);
+
+      if (!audioSrc) {
+        console.error("Audio source not found");
+        return;
+      }
+
+      setAudioSource(audioSrc);
+      setSelectedAudio(audio);
+    } catch (err) {
+      message.error("Could not play audio");
+      console.error(err);
+    }
+  };
+
+  return (
+    <Card title="Audio Files">
+      <Flex
+        justify="space-between"
+        align="center"
+        className={styles.topSection}
+      >
+        <Button onClick={fetchAudioFiles} className={styles.refreshButton}>
+          Refresh list <SyncOutlined />
+        </Button>
+        <Flex align="center" gap={24} className={styles.selectedAudio}>
+          {selectedAudio && (
+            <Text>
+              <b>Selected audio:</b> {selectedAudio.originalname}
+            </Text>
+          )}
+          <audio controls src={audioSource || undefined} />
+        </Flex>
+      </Flex>
+      <List
+        itemLayout="horizontal"
+        dataSource={audioFiles}
+        renderItem={(audio) => (
+          <List.Item
+            style={{
+              backgroundColor:
+                audio.id === selectedAudio?.id ? "#e6f7ff" : "white",
+              padding: "12px 16px",
+            }}
+          >
+            <List.Item.Meta
+              title={audio.originalname}
+              description={audio.description}
+            />
+            <Button onClick={() => handlePlay(audio)}>Select to play</Button>
+          </List.Item>
+        )}
+      />
+    </Card>
+  );
+};
+
+export default AudioList;
